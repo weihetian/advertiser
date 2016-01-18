2845aaa9beb538955171bb834d40e75dadff5e3a
weihetian <weihe@udel.edu>
2016-01-18 14:17:46 -0500

    update

--- js/_main.js
+++ js/_main.js
@@ -16,4 +16,34 @@ $("#double-label-slider")
     .slider("pips", {
         rest: "label",
         labels: doubleLabels
-    });
+});
+
+var startPos = 2;
+
+$("#double-label-slider").on("slidestop", function(event, ui) {
+      endPos = ui.value;
+
+      if (startPos != endPos) {
+          if(endPos==1){
+            $('.campaignoptions_header_item').hide();
+            $('.campaignoptions_header-1').show();
+
+            $('.story_content_campaignoptions_item').hide();
+            $('.story_content_campaignoptions_image-1').show();
+          }else if(endPos==2){
+            $('.campaignoptions_header_item').hide();
+              $('.campaignoptions_header-2').show();
+
+              $('.story_content_campaignoptions_item').hide();
+              $('.story_content_campaignoptions_image-2').show();
+          }else if(endPos==3){
+            $('.campaignoptions_header_item').hide();
+              $('.campaignoptions_header-3').show();
+
+              $('.story_content_campaignoptions_item').hide();
+              $('.story_content_campaignoptions_image-3').show();
+          }
+      }
+
+      startPos = endPos;
+  });
