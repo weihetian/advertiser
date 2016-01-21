821b7a8f5b3a95ee957e8cd65c4db8c5378ff98d
weihetian <weihe@udel.edu>
2016-01-20 21:57:59 -0500

    select circle update

--- js/_main.js
+++ js/_main.js
@@ -18,17 +18,35 @@ $("#double-label-slider")
         labels: doubleLabels
 });
 
+
 $(function() {
-   $( ".assembling_circle" ).draggable({
-     containment: "parent",
-     start: function() {
-        cleanpin();
-      },
-     stop: function() {
-        generatepin();
-      }
-    });
- });
+  $('.select_circle').show();
+  generatepin(300,100);
+});
+
+  $('.ipad_map_assembling').click(function(e) {
+    cleanpin();
+    $('.select_circle').hide();
+    var offset = $(this).offset();
+    $('.select_circle').css({top:(e.pageY - offset.top-175), left: (e.pageX - offset.left-175)});
+    $('.select_circle').show();
+    generatepin((e.pageX - offset.left-175),(e.pageY - offset.top-175));
+  })
+
+
+// $(function() {
+//
+//
+//    $( ".assembling_circle" ).draggable({
+//      containment: "parent",
+//      start: function() {
+//         cleanpin();
+//       },
+//      stop: function() {
+//         generatepin();
+//       }
+//     });
+//  });
 
  function cleanpin(){
    $('.assembling_pin').each(function(){
@@ -37,11 +55,11 @@ $(function() {
 
  }
 
- function generatepin(){
+ function generatepin(left,top){
    var pins="";
 
-   var circley = $(".assembling_circle").position().top;
-   var circlex = $(".assembling_circle").position().left;
+   var circley = $(".select_circle").position().top;
+   var circlex = $(".select_circle").position().left;
 
    var num = Math.floor(Math.random() * 15) + 15;
    for(var i=0;i<20; i++){
@@ -51,14 +69,14 @@ $(function() {
 
      if(distance < 170){
        console.log(distance);
-       pins+="<div class='assembling_pin' style='position:absolute; top:"+(y-60+circley)+"px;left:"+(x-10+circlex)+"px;'><img src='http://www.myiconfinder.com/uploads/iconsets/6096188ce806c80cf30dca727fe7c237.png'></div>";
+       pins+="<div class='assembling_pin' style='position:absolute; top:"+(y-60+top)+"px;left:"+(x-10+left)+"px;'><img src='http://www.myiconfinder.com/uploads/iconsets/6096188ce806c80cf30dca727fe7c237.png'></div>";
      }
 
    }
    $('.assembling_pins').html(pins);
    $('.assembling_pin').each(function(){
      var duration = Math.floor(Math.random() * 800) + 500 ;
-     $(this).transition({ opacity: 1, y:50,duration: duration})});
+     $(this).transition({ opacity: 1, y:50,duration: duration, delay: 1300})});
 
  }
 
